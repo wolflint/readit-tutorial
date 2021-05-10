e0358f62e0e806f27b579feca215ea00dab501e3
Marcin Wolf <wolflint@protonmail.com>
2021-05-10 10:36:24 +0100

    Wip class 5: get posts + create comments

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -61,7 +61,7 @@ const login = async (req: Request, res: Response) => {
             return res.status(401).json({ password: 'Password is incorrect' })
         }
 
-        const token = jwt.sign({ username }, process.env.JWT_SECRET)
+        const token = jwt.sign({ username }, process.env.JWT_SECRET!)
 
         res.set(
             'Set-Cookie',
@@ -77,7 +77,7 @@ const login = async (req: Request, res: Response) => {
         return res.json(user)
     } catch(err) {
         console.log(err);
-        return res.status(500).json({error: err})
+        return res.json({error: 'Something went wrong' })
     }
     
 }
